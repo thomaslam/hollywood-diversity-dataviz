5d31f1dac8c04b078d96023bd667c6c6b454bc3b
Thomas Lam <tuananhlam95@gmail.com>
2019-03-27 14:59:24 -0400

    added scraper, server, preprocess

---
+++ preprocess.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv('./datasets/movie_metadata.csv')
+print(df.shape)
+df.drop(['color', 'facenumber_in_poster', 'aspect_ratio'], axis=1)
+print(df.shape)
+        
