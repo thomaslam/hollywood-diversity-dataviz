e0f31ca56d9640220054d2e0f8852b92f6b618aa
Thomas Lam <tuananhlam95@gmail.com>
2019-04-11 15:52:33 -0400

    merged all movie dataframes

---
+++ merge_csvs.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+
+merged_df = pd.DataFrame()
+movies_dir = "./data/movies"
+for filename in os.listdir(movies_dir):
+    movie_df = pd.read_csv(movies_dir + "/" + filename)
+    print(filename)
+    merged_df = pd.concat([merged_df, movie_df], ignore_index=True)
+
+merged_df.to_csv("./data/movie_merged.csv")
